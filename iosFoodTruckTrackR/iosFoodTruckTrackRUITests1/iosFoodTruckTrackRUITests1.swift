6be8525f8fee6f473d98ea1f4c043419095a8200
brys <brys1saclaus2@gmail.com>
2020-11-15 12:52:08 -0800

    test 1/5

--- iosFoodTruckTrackR/iosFoodTruckTrackRUITests1/iosFoodTruckTrackRUITests1.swift
+++ iosFoodTruckTrackR/iosFoodTruckTrackRUITests1/iosFoodTruckTrackRUITests1.swift
@@ -8,21 +8,34 @@
 import XCTest
 
 class iosFoodTruckTrackRUITests1: XCTestCase {
+    var app: XCUIApplication! = nil
+    
+    var findAtruckTabBarButton: XCUIElement {
+        return app.tabBars["Tab Bar"].buttons["Find A Truck"]
+    }
+
 
     override func setUpWithError() throws {
-        
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        
+        app.terminate()
     }
 
     func testExample() throws {
-       
         let app = XCUIApplication()
         app.launch()
     }
     
+    func testToggleTabBarToFindATruck() throws {
+        XCTAssertTrue(findAtruckTabBarButton.isHittable)
+        findAtruckTabBarButton.tap()
+    }
+    
+    //func testAllowUserLocation
     
     
    
