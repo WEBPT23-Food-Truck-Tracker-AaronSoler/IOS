80aff6d69fd3da35dbb8fc3800694dbef26afbd3
brys <brys1saclaus2@gmail.com>
2020-11-15 13:53:51 -0800

    // testing UItableView cells

--- iosFoodTruckTrackR/iosFoodTruckTrackRUITests1/iosFoodTruckTrackRUITests1.swift
+++ iosFoodTruckTrackR/iosFoodTruckTrackRUITests1/iosFoodTruckTrackRUITests1.swift
@@ -10,11 +10,6 @@ import XCTest
 class iosFoodTruckTrackRUITests1: XCTestCase {
     var app: XCUIApplication! = nil
     
-    var findAtruckTabBarButton: XCUIElement {
-        return app.tabBars["Tab Bar"].buttons["Find A Truck"]
-    }
-
-
     override func setUpWithError() throws {
         continueAfterFailure = false
         app = XCUIApplication()
@@ -35,8 +30,43 @@ class iosFoodTruckTrackRUITests1: XCTestCase {
         findAtruckTabBarButton.tap()
     }
     
-    //func testAllowUserLocation
+    func testpresentWorldsBestFoodTruckCell() {
+        findAtruckTabBarButton.tap()
+        XCTAssertTrue(worldsBestFoodTructCell.isHittable)
+        worldsBestFoodTructCell.tap()
+    }
     
+    func testSomething() {findAtruckTabBarButton.tap()
+        XCTAssertTrue(corndogsOnTheRunCell.isHittable)
+        corndogsOnTheRunCell.tap()
+   }
+    
+    
+    
+    
+    
+
+    //MARK: - UI helpers
+    var findAtruckTabBarButton: XCUIElement {
+        return app.tabBars["Tab Bar"].buttons["Find A Truck"]
+    }
+    
+    var worldsBestFoodTructCell: XCUIElement {
+        return app.tables/*@START_MENU_TOKEN@*/.cells.matching(identifier: "operatorsTableView.operatorCell").staticTexts["Worlds Best Taco Truck, Period"]/*[[".cells.matching(identifier: \"operatorsTableView.operatorCell\").staticTexts[\"Worlds Best Taco Truck, Period\"]",".staticTexts[\"Worlds Best Taco Truck, Period\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+    }
+    
+    var corndogsOnTheRunCell: XCUIElement {
+        return app.tables.cells.matching(identifier: "operatorsTableView.operatorCell").staticTexts["Corndogs On The Run"]
+    }
+    
+    var allowWhileUsingAppPressed: XCUIElement {
+        return app.alerts["Allow “iosFoodTruckTrackR” to use your location?"].scrollViews.otherElements.buttons["Allow While Using App"]
+    }
+    
+    var alertAllowLocationOnceChosen: XCUIElement {
+        return app.alerts["Allow “iosFoodTruckTrackR” to use your location?"].scrollViews.otherElements.buttons["Allow Once"]
+    }
+        
     
    
 }
